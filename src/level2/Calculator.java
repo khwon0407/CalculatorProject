2da5fc9d96a66ed03c44646db1b399a51f1ccd80
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-04-29 19:34:08 +0900

    Lv 2 Todo 1 - 2
    implement calculate

--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -2,14 +2,39 @@ package level2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Calculator {
     //TODO 1. 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스
     List<Integer> resultCollection = new ArrayList<Integer>();
 
     //TODO 1. 사칙연산을 수행 후, 결과값 반환 메서드 구현
-    public int calculate(int first, int second, char op) {
-        int result = 0;
-        return result;
+    public Optional<Integer> calculate(int first, int second, char op) throws ArithmeticException {
+        Integer result = null;
+        Optional<Integer> resultOptional;
+
+        switch (op) {
+            case '+':
+                result = first + second;
+                break;
+            case '-':
+                result = first - second;
+                break;
+            case '*':
+                result = first * second;
+                break;
+            case '/':
+                try {
+                    result = first / second;
+                } catch (ArithmeticException e) {
+                    System.out.println("0으로는 나눌 수 없습니다.");
+                }
+                break;
+            default:
+                System.out.println("올바른 연산자를 입력해주세요.");
+        }
+
+        resultOptional = Optional.ofNullable(result);
+        return resultOptional;
     }
 }
