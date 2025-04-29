ed9d64b02e163bda6fc5718b05fa4e8c2e2f28ae
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-04-29 19:29:22 +0900

    Level 2 Todo 1 - 1
    make class, make method, add collection

---
+++ src/level2/Calculator.java
@@ -0,0 +1,15 @@
+package level2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    //TODO 1. 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스
+    List<Integer> resultCollection = new ArrayList<Integer>();
+
+    //TODO 1. 사칙연산을 수행 후, 결과값 반환 메서드 구현
+    public int calculate(int first, int second, char op) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/level2/CalculatorLv2.java
@@ -0,0 +1,7 @@
+package level2;
+
+public class CalculatorLv2 {
+    public static void main(String[] args) {
+
+    }
+}
